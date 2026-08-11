b5a38dc297ba551540616068a1a84a90c02eb388
Vadim <258285264+atlasru@users.noreply.github.com>
2026-08-12 00:51:33 +0300

    Wire libXray Android dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.dotclient.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -54,6 +54,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling:1.11.4")
 
     implementation("com.squareup.okhttp3:okhttp:5.1.0")
+    implementation(files("libs/libXray.aar"))
 
     testImplementation("junit:junit:4.13.2")
 }
