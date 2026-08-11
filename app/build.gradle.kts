46296f33e612ca0c02febf365e82a272027dc688
Vadim <258285264+atlasru@users.noreply.github.com>
2026-08-12 02:46:53 +0300

    Bump version to 0.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.dotclient.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.0.11"
+        versionCode = 12
+        versionName = "0.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
