2ae2de576795fe19268a0b26670895fd767cdf7a
Vadim <258285264+atlasru@users.noreply.github.com>
2026-08-12 22:04:23 +0300

    Bump Android to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.dotclient.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 104
-        versionName = "0.1.4"
+        versionCode = 105
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
