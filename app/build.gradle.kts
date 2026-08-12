56fcb319bca622a41b37294ba14dcf714404f69f
Vadim <258285264+atlasru@users.noreply.github.com>
2026-08-12 20:32:13 +0300

    Android 0.1.3: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.dotclient.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 102
-        versionName = "0.1.2"
+        versionCode = 103
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
