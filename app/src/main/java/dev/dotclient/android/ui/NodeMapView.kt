6bb108fd3948d83e0c8cab1685ebb55ef733a8ba
Yan <258285264+atlasru@users.noreply.github.com>
2026-08-14 00:21:02 +0300

    Add node map view scaffold

---
+++ app/src/main/java/dev/dotclient/android/ui/NodeMapView.kt
@@ -0,0 +1,8 @@
+package dev.dotclient.android.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NodeMapView(modifier: Modifier = Modifier) {
+}
