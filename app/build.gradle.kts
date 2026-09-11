93a2a6430c1f25a1bf01343bace062744c2953f6
Yan <258285264+atlasru@users.noreply.github.com>
2026-09-11 12:52:00 +0300

    Bump Android milestone to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.dotclient.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 108
-        versionName = "0.1.8"
+        versionCode = 200
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
